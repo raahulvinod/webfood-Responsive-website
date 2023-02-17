4832c682ade33d328c04100a0c76b3797c5c9895
Rahul vinod <rahulvinod135@gmail.com>
2023-02-17 12:13:28 +0530

    silde swiper

--- script.js
+++ script.js
@@ -28,3 +28,25 @@ const addClass = document.querySelector('.site');
 sTrigger.addEventListener('click', () => {
     addClass.classList.toggle('showsearch')
 })
+
+
+//-----------Slider-------------------
+const sliderThumb = new Swiper('.thumb-nav', {
+        spaceBetween: 10,
+        slidePerView: 3,
+        slidesPerGroup: false,
+        breakpoints: {
+            992: {
+                direction: 'vertical'
+            }
+        }
+  });
+  const theSlider = new Swiper('.thumb-big', {
+        slidePerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        thumbs: {
+            swiper: sliderThumb,
+        }
+});
