6fa3aa16fd866cd472ef4e6f4684bd6b7d65d16d
Rahul vinod <rahulvinod135@gmail.com>
2023-02-19 15:57:57 +0530

    on scroll animation added

--- script.js
+++ script.js
@@ -66,3 +66,17 @@ const theTab = new Swiper('.tabbed-item', {
         swiper: tabbedNav,
     }
 });
+
+//--------- On Scroll Transition
+const io = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('this')
+        }
+    })
+})
+
+const box = document.querySelectorAll('.animate');
+      box.forEach((el) => {
+        io.observe(el);
+      })
